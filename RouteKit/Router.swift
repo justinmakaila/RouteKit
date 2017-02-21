013c665ebd9438e15df10d1788e4b1f2dd65f11c
Justin Makaila <justinmakaila@gmail.com>
2017-02-21 16:31:23 -0500

    Added ACL

--- RouteKit/Router.swift
+++ RouteKit/Router.swift
@@ -2,14 +2,14 @@ import UIKit
 
 
 /// A type representing a route
-protocol RouteType { }
+public protocol RouteType { }
 
 extension String: RouteType { }
 
 
 /// A router is routes from a view controller of type `SourceViewController`
 /// to a route of type `Route`.
-protocol RouterType {
+public protocol RouterType {
     associatedtype Route: RouteType
     associatedtype SourceViewController: UIViewController
     
@@ -17,52 +17,52 @@ protocol RouterType {
 }
 
 
-protocol ContextualRouterType: RouterType {
+public protocol ContextualRouterType: RouterType {
     associatedtype Context
     var context: Context { get }
 }
 
 
-protocol RouterProvider {
+public protocol RouterProvider {
     associatedtype Router: RouterType
     var router: Router! { get }
 }
 
 
-class Router<Route: RouteType>: RouterType {
-    func route(to route: Route, source: UIViewController) { }
+open class Router<Route: RouteType>: RouterType {
+    open func route(to route: Route, source: UIViewController) { }
 }
 
-class ContextualRouter<Route: RouteType, Context>: Router<Route>, ContextualRouterType {
-    let context: Context
+public class ContextualRouter<Route: RouteType, Context>: Router<Route>, ContextualRouterType {
+    public let context: Context
     
-    init(context: Context) {
+    public init(context: Context) {
         self.context = context
     }
 }
 
 
-class DynamicRouter<Route: RouteType>: Router<Route> {
-    let route: (Route, UIViewController) -> ()
+public class DynamicRouter<Route: RouteType>: Router<Route> {
+    public let route: (Route, UIViewController) -> ()
     
-    init(route: @escaping ((Route, UIViewController) -> ())) {
+    public init(route: @escaping ((Route, UIViewController) -> ())) {
         self.route = route
     }
     
-    override func route(to route: Route, source: UIViewController) {
+    public override final func route(to route: Route, source: UIViewController) {
         self.route(route, source)
     }
 }
 
 class DynamicContextualRouter<Route: RouteType, Context>: ContextualRouter<Route, Context> {
-    let route: (Route, Context, UIViewController) -> ()
+    public let route: (Route, Context, UIViewController) -> ()
     
-    init(context: Context, route: @escaping ((Route, Context, UIViewController) -> ())) {
+    public init(context: Context, route: @escaping ((Route, Context, UIViewController) -> ())) {
         self.route = route
         super.init(context: context)
     }
     
-    override func route(to route: Route, source: UIViewController) {
+    public override final func route(to route: Route, source: UIViewController) {
         self.route(route, context, source)
     }
 }
